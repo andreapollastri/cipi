006310fe84dc0a2f3092f77ea36ab32cbdedf58c
Andrea Pollastri <scrivi@andreapollastri.net>
2018-09-23 10:16:59 +0200

    Update passwd.sh

--- passwd.sh
+++ passwd.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-
 USER=
 PASS=$(openssl rand -base64 32)
 DBPASS=$(openssl rand -base64 32)
-
 # Check if user is root
 if [ $(id -u) != "0" ]; then
     echo "Error: You must be root to run this script."
     exit 1
 fi
-
 while [ -n "$1" ] ; do
       case $1 in
       -u | --user )
               shift
               USER=$1
-              ;;                                                                
+              ;;
       * )
               echo "ERROR: Unknown option: $1"
               exit -1
@@ -23,17 +20,13 @@ while [ -n "$1" ] ; do
       esac
       shift
 done
-
 #CHANGE LINUX USER PASSWORD
 echo "$USER:$PASS"| sudo chpasswd
-
 #CHANGE MYSQL PASSWORD
 if [ -f "/cipi/$USER" ]
-then    
-
+then
     DBOLDPASS=$(for word in $(cat /cipi/$USER); do echo $word; done)
     sudo mysqladmin -u $USER -p'$DBOLDPASS' password '$DBPASS'
-
     #FINAL MESSAGGE
     clear
     echo "###################################################################################"
@@ -47,9 +40,7 @@ then
     echo ""
     echo "###################################################################################"
     echo ""
-
 else
-
     #FINAL MESSAGGE
     clear
     echo "###################################################################################"
@@ -62,5 +53,4 @@ else
     echo ""
     echo "###################################################################################"
     echo ""
-
 fi
