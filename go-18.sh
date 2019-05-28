c1e3880e4972b03a8ed551e9270d0b4d18bc3619
Andrea Pollastri <scrivi@andreapollastri.net>
2019-05-28 17:12:10 +0200

    Update go-18.sh

--- go-18.sh
+++ go-18.sh
@@ -40,6 +40,7 @@ sudo usermod -aG sudo $USER
 
 #PHP7 PPA
 sudo apt-get -y install python-software-properties
+sudo apt-get -y install software-properties-common
 sudo add-apt-repository -y universe
 sudo apt-get -y install software-properties-common
 sudo add-apt-repository -y ppa:ondrej/php
