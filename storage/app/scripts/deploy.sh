9443904d848751a8459927627ecee349526311d8
Andrea Pollastri <andrea.pollastri@qmi.it>
2019-12-08 19:55:49 +0100

    Deploy script fix

--- storage/app/scripts/deploy.sh
+++ storage/app/scripts/deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 ######### REMOTE GIT CONFIGURATION #########
-REPO="git@github.com:andreapollastri/cipi.git" #Use your Github
-BRANCH="master" #Choose your branch
+GITHUB="andreapollastri/cipi.git" #Choose your Github private/public repo
+BRANCH="master" #Choose your repository branch here
 
 ######### DO NOT CHANGE ANYTHING IN THIS AREA #########
+SSH_KEY="/home/###CIPI-USER###/git/deploy"
 WORK_TREE="/home/###CIPI-USER###/web"
 GIT_DIR="/home/###CIPI-USER###/git/deploy.git"
+chmod 600 $SSH_KEY
 eval $(ssh-agent -s)
+ssh-add $SSH_KEY
+REPO="git@github.com:$GITHUB"
 if [ -d "$GIT_DIR" ]; then
     cd $WORK_TREE
     git --work-tree=$WORK_TREE --git-dir=$GIT_DIR fetch
