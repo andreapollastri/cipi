71250bb458bf136f7ae12da288f821f326da5ff2
Andrea Pollastri <andrea.pollastri@qmi.it>
2019-12-10 16:55:40 +0100

    Laravel preinstallation fix

--- storage/app/scripts/hostadd.sh
+++ storage/app/scripts/hostadd.sh
@@ -312,9 +312,9 @@ if [ "$AUTO_INSTALL" = "laravel" ]; then
     find . -type f -exec chmod 644 {} \;
     find . -type d -exec chmod 755 {} \;
     chmod 777 -R /home/$USER_NAME/web/laravel/storage
-    sudo rpl -q "dbname" "$DBNAME" /home/$USER_NAME/web/laravel/.env
-    sudo rpl -q "dbuser" "$DBUSER" /home/$USER_NAME/web/laravel/.env
-    sudo rpl -q "dbpass" "$DBPASS" /home/$USER_NAME/web/laravel/.env
+    sudo rpl -q "DB_DATABASE=laravel" "DB_DATABASE=$DBNAME" /home/$USER_NAME/web/laravel/.env
+    sudo rpl -q "DB_USERNAME=root" "DB_USERNAME=$DBUSER" /home/$USER_NAME/web/laravel/.env
+    sudo rpl -q "DB_PASSWORD=" "DB_PASSWORD=$DBPASS" /home/$USER_NAME/web/laravel/.env
 fi
 
 #WORDPRESS
