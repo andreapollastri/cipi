efe6c46fbe9135033862d15ab9c9507b8ac509ea
Andrea Pollastri <scrivi@andreapollastri.net>
2020-04-11 18:32:46 +0200

    Main template + Dashboard + New Logo

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/home');
+});
+
+Route::get('/home', function () {
+    return view('dashboard');
 });
