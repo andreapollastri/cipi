997a26ba4aea394194b054ce2574d5658cee7f11
Andrea Pollastri <scrivi@andreapollastri.net>
2020-04-30 00:28:39 +0200

    Remote Controller Code Review

--- app/Http/Controllers/RemoteController.php
+++ app/Http/Controllers/RemoteController.php
@@ -8,9 +8,6 @@ class RemoteController extends Controller {
 
     public function start($servercode) {
         $server = Server::where('servercode', $servercode)->where('complete', 0)->first();
-        if(!$server) {
-            return abort(403);
-        }
         $server->complete = 1;
         $server->save();
         return 'OK';
@@ -18,9 +15,6 @@ class RemoteController extends Controller {
 
     public function finalize($servercode) {
         $server = Server::where('servercode', $servercode)->where('complete', 1)->first();
-        if(!$server) {
-            return abort(403);
-        }
         $server->complete = 2;
         $server->save();
         return 'OK';
@@ -28,19 +22,16 @@ class RemoteController extends Controller {
 
     public function ping($servercode) {
         $server = Server::where('servercode', $servercode)->where('complete', 2)->first();
-        if(!$server) {
-            return abort(403);
-        }
         $remote = Http::get('http://'.$server->ip.'/ping_'.$server->servercode.'.php');
         return $remote->status();
     }
 
     public function status($servercode) {
         $server = Server::where('servercode', $servercode)->where('complete', 2)->first();
-        if(!$server) {
-            return abort(403);
-        }
         $remote = Http::get('http://'.$server->ip.'/status_'.$server->servercode.'.php');
+        if($remote->status() != 200) {
+            return '--;--;--';
+        }
         return $remote->body();
     }
 
