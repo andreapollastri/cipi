11a53ba8f4019967e4a0f24b3eb17987c72a2904
Andrea Pollastri <scrivi@andreapollastri.net>
2020-05-04 19:09:09 +0200

    Migration fix

--- database/migrations/2020_04_27_000001_create_servers_table.php
+++ database/migrations/2020_04_27_000001_create_servers_table.php
@@ -18,7 +18,7 @@ class CreateServersTable extends Migration
             $table->string('name');
             $table->string('provider')->nullable();
             $table->string('location')->nullable();
-            $table->integer('ip');
+            $table->string('ip');
             $table->integer('port');
             $table->string('username');
             $table->string('password');
