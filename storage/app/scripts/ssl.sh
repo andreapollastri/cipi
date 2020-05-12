30211b606fb09beb7c6ca407c6fb635cb131a1bf
Andrea Pollastri <scrivi@andreapollastri.net>
2020-05-12 15:25:22 +0200

    http/2 fix

--- storage/app/scripts/ssl.sh
+++ storage/app/scripts/ssl.sh
@@ -21,8 +21,8 @@ while [ -n "$1" ] ; do
 done
 
 sudo certbot --nginx -d $DOMAIN --non-interactive --agree-tos --register-unsafely-without-email
-sudo rpl -i -w "[::]:443 ssl" "[::]:443 ssl http2" /etc/nginx/sites-enabled/$CONFIG.conf
-sudo rpl -i -w "443 ssl" "443 ssl http2" /etc/nginx/sites-enabled/$CONFIG.conf
+sudo systemctl restart nginx.service
+sed -i 's/443 ssl/443 ssl http2/g' /etc/nginx/sites-enabled/$CONFIG.conf
 sudo systemctl restart nginx.service
 
 
