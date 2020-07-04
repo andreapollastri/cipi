6c58416988969987c9c383d56e0068ed55df3483
Andrea Pollastri <scrivi@andreapollastri.net>
2020-07-05 00:50:48 +0200

    Update 18.sh

--- setup/18.sh
+++ setup/18.sh
@@ -64,8 +64,7 @@ echo "Server basic configuration..."
 sleep 3s
 
 sudo apt-get update
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -y install rpl zip unzip openssl curl dirmngr apt-transport-https lsb-release ca-certificates dnsutils htop
+sudo apt-get -y install rpl zip unzip curl dirmngr apt-transport-https lsb-release ca-certificates dnsutils htop
 
 
 sudo rpl -i -w "#PasswordAuthentication" "PasswordAuthentication" /etc/ssh/sshd_config
