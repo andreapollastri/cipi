1ad5122394e236e8eafdc4a523588422e662d743
Andrea Pollastri <scrivi@andreapollastri.net>
2020-07-05 00:41:35 +0200

    Update 18.sh

--- setup/18.sh
+++ setup/18.sh
@@ -64,6 +64,7 @@ echo "Server basic configuration..."
 sleep 3s
 
 sudo apt-get update
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -y install rpl zip unzip openssl curl dirmngr apt-transport-https lsb-release ca-certificates dnsutils htop
 
 
