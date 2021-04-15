f8025b78bc63430e498f6d76769a7b3d90e4d9a2
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2021-04-15 19:56:13 +0200

    Installation Count

---
+++ app/Console/Commands/ActiveSetupCount.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Server;
+use phpseclib3\Net\SSH2;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class ActiveSetupCount extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cipi:activesetupcount';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Active Setup Count';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        sleep(rand(0,9));
+
+        // Useful to daily count how many Cipi Control Panel installations are active
+        // around the world and show the total value into cipi.sh official website
+        file_get_contents(config('cipi.activesetupcount'));
+
+        return 0;
+    }
+}
