540305e711ae4f41f6e00c33dac0b5be9f087a28
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2021-04-28 18:07:41 +0200

    Cipi Basic Tests

---
+++ tests/Feature/CipiTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CipiTest extends TestCase
+{
+  
+    public function testRedirectToLogin()
+    {
+        $this->get('/')
+        $response->assertRedirect('/login');
+    }
+
+    public function testShowLoginPage()
+    {
+        $response = $this->get('/login');
+        $response->assertSee('Cipi Control Panel');
+        $response->assertStatus(200);
+    }
+  
+    public function testDefaultLogin()
+    {
+        $this->visit('/login')
+             ->type('administrator', 'username')
+             ->type('12345678', 'password')
+             ->press('OK')
+             ->seePageIs('/dashboard');
+    }
+
+}
