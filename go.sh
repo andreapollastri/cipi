af06fd3abd97913a3ddad69854e0c28f026b1dd4
Andrea Pavone <info@andreapavone.com>
2023-02-06 21:49:55 +0100

    Add php8.2 support

--- go.sh
+++ go.sh
@@ -380,7 +380,7 @@ sudo apt-get -y install php8.2-imagick
 sudo apt-get -y install php8.2-fileinfo
 sudo apt-get -y install php8.2-imap
 sudo apt-get -y install php8.2-cli
-PHPINI=/etc/php/8.1/fpm/conf.d/cipi.ini
+PHPINI=/etc/php/8.2/fpm/conf.d/cipi.ini
 sudo touch $PHPINI
 sudo cat > "$PHPINI" <<EOF
 memory_limit = 256M
