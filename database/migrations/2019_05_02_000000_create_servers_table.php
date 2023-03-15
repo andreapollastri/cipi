bdb15f391f1676cd6ebc77fbd5221d5f38e8453e
Shift <shift@laravelshift.com>
2023-03-15 13:34:23 +0000

    Adopt anonymous migrations

--- database/migrations/2019_05_02_000000_create_servers_table.php
+++ database/migrations/2019_05_02_000000_create_servers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateServersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -41,4 +41,4 @@ class CreateServersTable extends Migration
     {
         Schema::dropIfExists('servers');
     }
-}
+};
