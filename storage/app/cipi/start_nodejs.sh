11867b73c14e93281ba1d0750b220f1446bb6814
SamjiDiamond <odejinmisamuel@gmail.com>
2023-06-19 14:21:45 +0100

    added start up script for Nodejs site

---
+++ storage/app/cipi/start_nodejs.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+PHP=
+USER_NAME=
+
+while [ -n "$1" ] ; do
+    case $1 in
+    -u | --user* )
+        shift
+        USER_NAME=$1
+        ;;
+    -p | --port* )
+        shift
+        PORT=$1
+        ;;
+    -ph | --path )
+        shift
+        PATH=$1
+        ;;
+    * )
+        echo "ERROR: Unknown option: $1"
+        exit -1
+        ;;
+    esac
+    shift
+done
+
+
+cd /home/$USER_NAME
+npm install
+sudo cat > ecosystem.config.js <<EOF
+module.exports = {
+  apps : [{
+    name   : "$USER_NAME",
+    script : "./$PATH",
+    instances : "1",
+    exec_mode : "fork",
+    env: {
+      NODE_ENV: "production",
+      PORT: $PORT,
+    },
+    log_file: "/home/$USER_NAME/log",
+    max_restarts: 3
+  }]
+}
+
+EOF
+
+pm2 start ecosystem.config.js
+
+#pm2 start npx --name "$USER_NAME" -- next -p $PORT -l "/home/$USER_NAME/log"
