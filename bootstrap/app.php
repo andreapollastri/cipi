797b5953cd339c22864ef17bfc375fd06f9d6b1b
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2024-03-16 00:41:00 +0100

    Check Server Status

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,7 +9,7 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        // health: '/up',
+        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
